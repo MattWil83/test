11c064171e9bdb58f9925af35a23f86367c7d51b
Dino Saur <mwilomovsky83@gmail.com>
2017-04-19 10:58:19 -0400

    5

--- src/Project/Assembler.java
+++ src/Project/Assembler.java
@@ -1,7 +1,21 @@
 package Project;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Assembler {
 	public static void assemble(File input, File output, ArrayList<String> errors){
+		ArrayList<String> text=new ArrayList<>();
+		try (Scanner inp = new Scanner(input)) {
+			while(inp.hasNextLine()) {
+				text.add(inp.nextLine()); 
+			}
+		} catch (FileNotFoundException e) { 
+			errors.add(0, "Error: Unable to open the input file"); 
+			return;
+		}
 		
 	}
 }
