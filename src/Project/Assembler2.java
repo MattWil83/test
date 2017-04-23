21a186b0ce8e904db42558f1779e0bacf54f60b2
Dino Saur <Dino Saur>
2017-04-23 15:07:47 -0400

    2

--- src/Project/Assembler2.java
+++ src/Project/Assembler2.java
@@ -13,10 +13,8 @@ public class Assembler2 {
 		ArrayList<String> intext=new ArrayList<>();
 		String temp;
 		try (Scanner inp = new Scanner(input)) {
-			temp = inp.nextLine();
 			while(inp.hasNextLine()){
-				intext.add(temp);
-				temp = inp.nextLine();}
+				intext.add(inp.nextLine());}
 		} catch (FileNotFoundException e) { 
 			errors.add(0, "Error: Unable to open the input file"); 
 			return;}
@@ -30,66 +28,93 @@ public class Assembler2 {
 				errors.add("Error: line " + (intext.indexOf(temp2)+1) + " starts with white space");}
 
 			if(temp2.trim().toUpperCase().startsWith("--")){
-				if(temp2.trim().replace("-", "").length()==0){
-
+				if(temp2.trim().replace("-", "").length()!=0){
+					errors.add("Error: line " + (intext.indexOf(temp2)+1) + " has a badly formatted data separator");
 				}
 			}}
+		if(errors.size()<1){
+			int dash=0;
+			while(!intext.get(dash).startsWith("--")){
+				code.add(intext.get(dash));
+				dash++;}
+			for(int i=dash+1;i<intext.size();i++){
+				data.add(intext.get(i));
+			}
 
-		for(String line : intext){
-			if(!line.startsWith("--")){
-				code.add(line);}
-			else{
-				data.add(line);}
-		}
-		ArrayList<String>outtext=new ArrayList<>();
+			ArrayList<String>outtext=new ArrayList<>();
 
-		for(String line:code){
-			String [] parts = line.trim().split("\\s+");
-			int lvl=0;
-			if(InstructionMap.sourceCodes.contains(parts[0].toUpperCase())&&!InstructionMap.sourceCodes.contains(parts[0])){
-				errors.add("Error: line " + (intext.indexOf(line)+1) + " does not have the instruction mnemonic in upper case");}
+			for(String line:code){
+				String [] parts = line.trim().split("\\s+");
+				int lvl=0;
+				if(InstructionMap.sourceCodes.contains(parts[0].toUpperCase())&&!InstructionMap.sourceCodes.contains(parts[0])){
+					errors.add("Error: line " + (intext.indexOf(line)+1) + " does not have the instruction mnemonic in upper case");}
 
-			else if(InstructionMap.noArgument.contains(parts[0])&&parts.length==1)
-				errors.add("Error: line " + (intext.indexOf(line)+1) + " has an illegal argument");
+				else if(InstructionMap.noArgument.contains(parts[0])&&parts.length!=1)
+					errors.add("Error: line " + (intext.indexOf(line)+1) + " has an illegal argument");
 
-			if(!InstructionMap.noArgument.contains(parts[0])&&parts.length==1)
-				errors.add( "Error: line " + (intext.indexOf(line)+1) + " is missing an argument");
+				else if(!InstructionMap.noArgument.contains(parts[0])&&parts.length==1)
+					errors.add( "Error: line " + (intext.indexOf(line)+1) + " is missing an argument");
 
-			else if(parts.length>=3)
-				errors.add( "Error: line " + (intext.indexOf(line)+1) + " has more than one argument");
+				else if(parts.length>=3)
+					errors.add( "Error: line " + (intext.indexOf(line)+1) + " has more than one argument");
 
-			if(parts.length==2&&parts[1].startsWith("[")&&!InstructionMap.indirectOK.contains(parts[0])){
-				errors.add("Error: line " + (intext.indexOf(line)+1) + " has an illegal argument");}
+				else if(parts.length==2&&parts[1].startsWith("[")&&!InstructionMap.indirectOK.contains(parts[0])){
+					errors.add("Error: line " + (intext.indexOf(line)+1) + " has an illegal argument");}
 
-			else if(parts.length==2&&parts[1].startsWith("[")&&InstructionMap.indirectOK.contains(parts[0])){
-				if(!parts[0].endsWith("]"))
-					errors.add("Error: line " + (intext.indexOf(line)+1) + " Does not end with ]");}
+				else if(parts.length==2&&parts[1].startsWith("[")&&InstructionMap.indirectOK.contains(parts[0])){
+					if(!parts[1].endsWith("]"))
+						errors.add("Error: line " + (intext.indexOf(line)+1) + " Does not end with ]");
+					else if(parts[0].endsWith("]")){
+						parts[0].substring(1, parts[0].length()-1);
+						lvl=2;}}
 
+//				else if(parts.length!=2)
+//					errors.add("Error: line " + (intext.indexOf(line)+1) + " Instruction missing argument");
+
+				System.out.println(parts[0]);
+				int arg = 0; 
+//							try {
+//								arg = Integer.parseInt(parts[1],16);
+//							} catch (NumberFormatException e) {
+//								errors.add("Error: line " + (intext.indexOf(line)+1) + " does not have a numeric argument");
+//							}
 			
-			else if(parts[0].endsWith("A"))
-				lvl=3;
+			}
+			for(String line:code){
+				String[] parts = line.trim().split("\\s+");
+				int lvl=0;
+				if(parts.length==2){
+					lvl=1;
+					if(parts[1].startsWith("[")){
+						lvl=2;
+						parts[1]=parts[1].substring(1, parts[1].length()-1);}}
+				if(parts[0].endsWith("I")){
+					lvl=0;}
+				else if(parts[0].endsWith("A"))
+					lvl=3;
 
-			int opcode = InstructionMap.opcode.get(parts[0]);
-			if(parts.length==1)
-				outtext.add(Integer.toHexString(opcode).toUpperCase() + " 1 0");
-			if(parts.length==2)
-				outtext.add(Integer.toHexString(opcode).toUpperCase() + " " + lvl + " " + parts[1]);
-
-		}
-		outtext.add("-1");
-		outtext.addAll(data);
-
-		try (PrintWriter out = new PrintWriter(output)){
-			for(String s : outtext) out.println(s);
-		} catch (FileNotFoundException e) {
-			errors.add("Cannot create output file");
-		}
-
-	}
+				int opcode = InstructionMap.opcode.get(parts[0]);
+				if(parts.length==1)
+					outtext.add(Integer.toHexString(opcode).toUpperCase() + " 1 0");
+				if(parts.length==2)
+					outtext.add(Integer.toHexString(opcode).toUpperCase() + " " + lvl + " " + parts[1]);}
+			
+			
+			System.out.println(errors.toString());
+			outtext.add("-1");
+			outtext.addAll(data);
+			
+			if(errors.size()<1){
+				try (PrintWriter out = new PrintWriter(output)){
+					for(String s : outtext) out.println(s);
+				} catch (FileNotFoundException e) {
+					errors.add("Cannot create output file");}}}
+		
+		System.out.println(data.toString());}
+	
 	public static void main(String[] args) {
 		ArrayList<String> errors = new ArrayList<>();
-		assemble(new File("in.pasm"), new File("out.pexe"), errors);		
-	}
+		assemble(new File("in.pasm"), new File("out.pexe"), errors);}
 
 
 }
