62475afef39877900586fe1e920539d8b2fcd4d0
Dino Saur <Dino Saur>
2017-04-23 13:15:39 -0400

    2

--- src/Project/Assembler2.java
+++ src/Project/Assembler2.java
@@ -8,52 +8,33 @@ import java.util.Scanner;
 
 public class Assembler2 {
 	public static void assemble(File input, File output, ArrayList<String> errors){
-		ArrayList<String> code=new ArrayList<>();
-		ArrayList<String> data=new ArrayList<>();
-		String temp;
+		ArrayList<String> intext=new ArrayList<>();
 		try (Scanner inp = new Scanner(input)) {
 			while(inp.hasNextLine()){
-				temp=inp.nextLine();
-				while(!temp.startsWith("--")){
-					code.add(temp);
-					temp=inp.nextLine();}
-				while(inp.hasNextLine()){
-					data.add(inp.nextLine());
-				}
-			}
+				intext.add(inp.nextLine());}
 		} catch (FileNotFoundException e) { 
 			errors.add(0, "Error: Unable to open the input file"); 
 			return;}
-		ArrayList<String>outtext=new ArrayList<>();
 
-		for(String line:code){
-			String[] parts = line.trim().split("\\s+");
-			int lvl=0;
-			if(parts.length==2){
-				lvl=1;
-				if(parts[1].startsWith("[")){
-					lvl=2;
-					parts[1]=parts[1].substring(1, parts[1].length()-1);}}
-			if(parts[0].endsWith("I")){
-				lvl=0;}
-			else if(parts[0].endsWith("A"))
-				lvl=3;
-
-			int opcode = InstructionMap.opcode.get(parts[0]);
-			if(parts.length==1)
-				outtext.add(Integer.toHexString(opcode).toUpperCase() + " 1 0");
-			if(parts.length==2)
-				outtext.add(Integer.toHexString(opcode).toUpperCase() + " " + lvl + " " + parts[1]);
-
+		for(String temp:intext){
+			if (temp.trim().length()==0){
+				if(temp.trim().length() > 0){
+					errors.add("Error: line " + intext.indexOf(temp) + " is a blank line");}}
+			if(temp.charAt(0)== ' ' ||temp.charAt(0)== '\t'){
+				errors.add("Error: line " + intext.indexOf(temp) + " starts with white space");}
+			if(temp.trim().toUpperCase().startsWith("--")){
+				if(temp.trim().replace("-", "").length()==0){
+					
+				}
+			}
+			
+			
+			
 		}
-		outtext.add("-1");
-		outtext.addAll(data);
 
-		try (PrintWriter out = new PrintWriter(output)){
-			for(String s : outtext) out.println(s);
-		} catch (FileNotFoundException e) {
-			errors.add("Cannot create output file");
-		}
+		
+
+		
 
 	}
 }
